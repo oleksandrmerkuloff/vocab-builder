a227080f3bb119fce2359f175a05404d47e0f174
oleksandrmerkuloff <oleksandrmerkuloff@gmail.com>
2025-09-19 23:16:17 +0300

    add dll support for Windows

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os, sys
+
 from application.app import App
 
+
 if __name__ == '__main__':
+    if getattr(sys, 'frozen', False):  # коли запускається .exe
+        os.add_dll_directory(os.path.dirname(sys.executable))
     app = App()
     app.mainloop()
--- utils/word_card.py
+++ utils/word_card.py
@@ -1,3 +1,9 @@
+import os
+
+
+os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin")
+
+
 from weasyprint import HTML
 
 
